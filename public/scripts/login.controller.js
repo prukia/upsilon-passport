a10eaeea176e20edeceff52052111346f484ce0f
Ryan Mulcahy <ryan@primeacademy.io>
2017-02-08 10:24:38 -0600

    Setup for lecture

---
+++ public/scripts/login.controller.js
@@ -0,0 +1,19 @@
+angular.module('passportApp')
+.controller('LoginController', LoginController);
+
+function LoginController($http, $location) {
+  console.log('LoginController loaded');
+  var ctrl = this;
+
+  ctrl.login = function() {
+    console.log('logging in');
+    $http.post('/login', {
+      username: ctrl.username,
+      password: ctrl.password
+    }).then(function(response){
+      console.log(response);
+    }, function(error) {
+      console.log('error loggin in', error);
+    });
+  };
+}
